4dd86a3aa5cbe1edbc75f742f9e90366c0616fc5
Lenny Rachitsky <lennysan@gmail.com>
2012-04-15 17:44:52 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bedsurfing::Application.config.secret_token = '89f8ca2b5e7206aea5056c134c59b0500e3b60c76ca2f2d10ea2b75a0b43c664e37b57ec136f82131c29846b89f8987d6cb9d7da7aa30fb0ba86e12644173fab'
